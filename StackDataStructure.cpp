fe025231cf489070cf4d64ce49256c5f5be97a85
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 20:32:17 -0700

    --Key functions of a stack in Cpp

--- StackDataStructure.cpp
+++ StackDataStructure.cpp
@@ -1,17 +1,45 @@
 
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 int main() {
 
+    stack<int>numbersStack;
+
+    // empty, size, push, pop, top
+
+    if(numbersStack.empty())
+        cout << "Stack is empty.";
+    else 
+        cout << "Stack is not empty.";    
+ 
 
 
-    system("pause>0");
-
+    return 0;
 
 }
 
 // MARK: - Key Notes:
 
     // Stacks are LIFO
+        // Last in First out
+
+    // FILO 
+
+// MARK: - Five Key functions with Stack
+
+    // #1: 'Empty'
+
+
+    // #2: 'Size'
+
+
+    // #3: 'Top'
+
+
+    // #4: 'Push'
+
+
+    // #5: 'Pop'
