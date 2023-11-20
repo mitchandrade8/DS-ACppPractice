7bf208d5b6603567920eae5a5f2fdbac5bceda31
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-20 11:15:14 -0700

    --Looping through to add elements to array

--- ArraysinCpp/ArrayDataStructure.cpp
+++ ArraysinCpp/ArrayDataStructure.cpp
@@ -7,6 +7,7 @@ int main() {
 
     // total, average, inTwoYears
 
+    // MARK: - First Version: Hard Coded:
     // float month1 = 100;
     // float month2 = 220;
     // float month3 = 300;
@@ -17,15 +18,21 @@ int main() {
     // Array 
     float monthsArray[6]; // = { 100, 220, 300, 0, 200, 250} ;
 
-    // Allow the user to enter the amount for each month:
-    cout << "Enter amount: ";
-    cin >> monthsArray[0];
-    cin >> monthsArray[1];
-    cin >> monthsArray[2];
-    cin >> monthsArray[3];
-    cin >> monthsArray[4];
-    cin >> monthsArray[5];
-    
+    // MARK: - Allow the user to enter the amount for each month:
+    // cout << "Enter amount: ";
+    // cin >> monthsArray[0];
+    // cin >> monthsArray[1];
+    // cin >> monthsArray[2];
+    // cin >> monthsArray[3];
+    // cin >> monthsArray[4];
+    // cin >> monthsArray[5];
+
+    // MARK: - Using a for loop:
+    for (int i = 0; i <= 5; i++) {
+        cout << "Please enter amount for element " << i << endl;
+        cin >> monthsArray[i];
+    }
+
 
     float total = monthsArray[0] + monthsArray[1] + monthsArray[2] + monthsArray[3] + monthsArray[4] + monthsArray[5];
 
