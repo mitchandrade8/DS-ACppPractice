6a63298d8bf5bf7dc002853c0e55279e10a0eadd
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-20 11:01:10 -0700

    --Continuing Arrays DS in Cpp

--- ArraysinCpp/ArrayDataStructure.cpp
+++ ArraysinCpp/ArrayDataStructure.cpp
@@ -7,14 +7,18 @@ int main() {
 
     // total, average, inTwoYears
 
-    float month1 = 100;
-    float month2 = 220;
-    float month3 = 300;
-    float month4 = 0;
-    float month5 = 200;
-    float month6 = 250; 
+    // float month1 = 100;
+    // float month2 = 220;
+    // float month3 = 300;
+    // float month4 = 0;
+    // float month5 = 200;
+    // float month6 = 250;
 
-    float total = month1 + month2 + month3 + month4 + month5 + month6;
+    // Array 
+    float monthsArray[6] = { 100, 220, 300, 0, 200, 250} ;
+
+
+    float total = monthsArray[0] + monthsArray[1] + monthsArray[2] + monthsArray[3] + monthsArray[4] + monthsArray[5];
 
     float average = total/6;
 
